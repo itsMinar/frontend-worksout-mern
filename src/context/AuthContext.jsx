a82cfa02aa0f6c8192177642db11173d2b8ccf42
Md. Minar Munshi <minarmunshi46@gmail.com>
2022-11-11 00:18:04 +0600

    Set the initial Auth status

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { createContext, useReducer } from 'react';
 
 export const AuthContext = createContext();
@@ -18,6 +19,14 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null,
     });
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        if (user) {
+            dispatch({ type: 'LOGIN', payload: user });
+        }
+    }, []);
 
     console.log('AuthContext state: ', state);
 
